7ff6f60a65ec438934a35349472049300305626e
unknown <amazighmalek.com>
2024-04-17 10:37:29 +0200

    install nexjs and anther package

---
+++ app/profile/page.tsx
@@ -0,0 +1,13 @@
+// http://localhost:3000/profile
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-center my-7 font-semibold'>
+      Profile Page
+    </div>
+  )
+}
+
+export default page
