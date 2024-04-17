6ecac2c60865f67d3353dc68f755575a74b9441c
unknown <amazighmalek.com>
2024-04-17 12:10:41 +0200

    dark mood features theme

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,12 +1,24 @@
 // http://localhost:3000/profile
 
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+
 import React from 'react'
 
 const page = () => {
   return (
+    <>
     <div className='text-center my-7 font-semibold'>
       Profile Page
     </div>
+    <div className='container'>
+       
+    <Input /> 
+    <Button className='mt-3'>Clike me</Button>
+    </div>
+    
+    </>
+    
   )
 }
 
